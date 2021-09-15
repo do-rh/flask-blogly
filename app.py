e8f3b2129ba339222397d4af6008ebf5c891ad63
Jenny Liu <jennyliuye@gmail.com>
2021-09-15 12:51:15 -0700

    fix flask run

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 """Blogly application."""
 
 from flask import Flask, redirect, render_template, request
-# from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'this-is-my-top-level-secret'
-# debug = DebugToolbarExtension(app)
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = 'this-is-my-top-level-secret'
+debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# db.create_all()
 
 @app.get('/')
 def home_page():
