749980b021c2e62fc3c595f54060325d552b276e
Jenny Liu <jennyliuye@gmail.com>
2021-09-15 12:33:44 -0700

    add base route for showing users

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template, request
+# from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'this-is-my-top-level-secret'
+# debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.get('/')
+def home_page():
+    return redirect('/users')
+
+@app.get('/users')
+def show_all_users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+
